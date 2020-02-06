a6eebac228b342f8a2f3e40ddfca7ddd77083311
Pacutacatete100 <pacutacatete100@gmail.com>
2020-02-05 21:12:50 -0600

    1

--- bot/ScienceDaily.py
+++ bot/ScienceDaily.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup as soup
 from Article import Article
 
 science_daily_AI_url = 'https://www.sciencedaily.com/news/computers_math/artificial_intelligence'
-science_daily = "https://www.sciencedaily.com"
+science_daily = 'https://www.sciencedaily.com'
 
 
 def get_page_html(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
-    page_soup = soup(webpage, "html.parser")
+    page_soup = soup(webpage, 'html.parser')
     return page_soup
 
 
@@ -18,8 +18,8 @@ def scrape_for_top_title():
     page_soup = get_page_html(
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
-    title_html_element = page_soup.find("div", {"id": "featured_tab_1"})
-    title = title_html_element.find("h3", "latest-head").text
+    title_html_element = page_soup.find('div', {'id': 'featured_tab_1'})
+    title = title_html_element.find('h3', 'latest-head').text
 
     return title
 
@@ -28,17 +28,17 @@ def scrape_for_top_sum():
     page_soup = get_page_html(
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
-    title_html_element = page_soup.find("div", {"id": "featured_tab_1"})
+    title_html_element = page_soup.find('div', {'id': 'featured_tab_1'})
 
     summary_html_element = title_html_element.find(
-        "div", "latest-summary").find("span", "more").find("a")
-    summary_link = summary_html_element["href"]
+        'div', 'latest-summary').find('span', 'more').find('a')
+    summary_link = summary_html_element['href']
     more_url = science_daily + summary_link
 
     page_soup2 = get_page_html(more_url)
 
-    summary = page_soup2.find("div", {"id": "story_text"}).find(
-        "p", {"id": "first"}).text
+    summary = page_soup2.find('div', {'id': 'story_text'}).find(
+        'p', {'id': 'first'}).text
 
     return summary
 
@@ -47,16 +47,16 @@ def scrape_for_top_link():
     page_soup = get_page_html(
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
-    title_html_element = page_soup.find("div", {"id": "featured_tab_1"})
+    title_html_element = page_soup.find('div', {'id': 'featured_tab_1'})
 
     summary_html_element = title_html_element.find(
-        "div", "latest-summary").find("span", "more").find("a")
-    summary_link = summary_html_element["href"]
+        'div', 'latest-summary').find('span', 'more').find('a')
+    summary_link = summary_html_element['href']
     more_url = science_daily + summary_link
 
     page_soup2 = get_page_html(more_url)
-    link_ref = page_soup2.find("div", {"id": "story_source"}).find("a")
-    link = link_ref["href"]
+    link_ref = page_soup2.find('div', {'id': 'story_source'}).find('a')
+    link = link_ref['href']
 
     return link
 
@@ -74,26 +74,26 @@ def scrape_for_search(*args):
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
     latest_summs_div = page_soup.find(
-        "div", {"id": "summaries"}).find_all("div", "latest-summary")
+        'div', {'id': 'summaries'}).find_all('div', 'latest-summary')
 
     latest_heads = page_soup.find(
-        "div", {"id": "summaries"}).find_all("h3", "latest-head")
+        'div', {'id': 'summaries'}).find_all('h3', 'latest-head')
 
     for sums in latest_summs_div:
         more_URLs.append(
-            science_daily + sums.find("span", "more").find("a")["href"])
+            science_daily + sums.find('span', 'more').find('a')['href'])
 
     for more in more_URLs:
         page_soup_2 = get_page_html(more)
 
-        headline = page_soup_2.find("h1", "headline").text
+        headline = page_soup_2.find('h1', 'headline').text
         headlines.append(headline.lower())
 
-        summary = page_soup_2.find("dd", {"id": "abstract"}).text
+        summary = page_soup_2.find('dd', {'id': 'abstract'}).text
         summaries.append(summary)
 
         source = page_soup_2.find(
-            "div", {"id": "story_source"}).find("a")["href"]
+            'div', {'id': 'story_source'}).find('a')['href']
         sources.append(source)
 
     for h, s, l in zip(headlines, summaries, sources):
@@ -106,4 +106,4 @@ def scrape_for_search(*args):
     return final_articles
 
 
-scrape_for_search("robot")  # for testing only
+scrape_for_search('robot')  # for testing only
