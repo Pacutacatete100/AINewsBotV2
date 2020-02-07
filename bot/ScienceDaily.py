66e3b7db6b9229a870e757668c556b8d29e6d882
Pacutacatete100 <pacutacatete100@gmail.com>
2020-02-06 22:23:56 -0600

    finished rewriting in requests, finished search

--- bot/ScienceDaily.py
+++ bot/ScienceDaily.py
@@ -1,5 +1,4 @@
 import requests
-from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from Article import Article
 
@@ -22,7 +21,7 @@ def scrape_for_top_title():
 
     title_html_element = page_soup.find(id='featured_tab_1')
 
-    print(title_html_element.h3.a.text)
+    return title_html_element.h3.a.text
 
 
 def scrape_for_top_sum():
@@ -31,46 +30,34 @@ def scrape_for_top_sum():
 
     title_html_element = page_soup.find(id='featured_tab_1')
 
+    # print(title_html_element)
+
     more_link = title_html_element.find(
         class_='latest-summary').find(class_='more').a['href']
 
     more = science_daily + more_link
 
-    soup2 = get_page_html(more)
+    soup_2 = get_page_html(more)
 
-    # summary = soup2.find(id='story_text')
-
-    print(soup2)
-
-    # summary_html_element = title_html_element.find(
-    #     'div', class_='latest-summary').find('span', class_='more').find('a')
-    # summary_link = summary_html_element['href']
-    # more_url = science_daily + summary_link
-
-    # page_soup2 = get_page_html(more_url)
-
-    # summary = page_soup2.find('div', id='story_text').find(
-    #     'p', id='first').text
-
-    # return summary
+    return soup_2.find(id='abstract').text
 
 
 def scrape_for_top_link():
     page_soup = get_page_html(
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
-    title_html_element = page_soup.find('div', id='featured_tab_1')
+    title_html_element = page_soup.find(id='featured_tab_1')
 
     summary_html_element = title_html_element.find(
-        'div', class_='latest-summary').find('span', class_='more').find('a')
+        'div', class_='latest-summary').find('span', class_='more').a
+
     summary_link = summary_html_element['href']
     more_url = science_daily + summary_link
 
     page_soup2 = get_page_html(more_url)
-    link_ref = page_soup2.find('div', id='story_source').find('a')
-    link = link_ref['href']
+    link_ref = page_soup2.find(id='story_source').a['href']
 
-    return link
+    return link_ref
 
 
 def scrape_for_search(*args):
@@ -86,25 +73,32 @@ def scrape_for_search(*args):
     page_soup = get_page_html(
         'https://www.sciencedaily.com/news/computers_math/artificial_intelligence')
 
-    latest_summs_div = page_soup.find(
-        'div', id='summaries').find_all('div', class_='latest-summary')
+    latest_summs_div = page_soup.find(id='summaries').find_all(
+        'div', class_='latest-summary')
 
-    latest_heads = page_soup.find(
-        'div', id='summaries').find_all('h3', class_='latest-head')
+    featured_summs = page_soup.find(id='featured_shorts').find_all('li')
+
+    latest_heads = page_soup.find(id='summaries').find_all(
+        'h3', class_='latest-head')
 
     for sums in latest_summs_div:
-        more_URLs.append(
-            science_daily + sums.find('span', class_='more').find('a')['href'])
+        URL = science_daily + sums.find('span', class_='more').a['href']
+
+        more_URLs.append(URL)
+
+    for sums in featured_summs:
+        URL = science_daily + sums.find('a')['href']
+        more_URLs.append(URL)
 
     for more in more_URLs:
         page_soup_2 = get_page_html(more)
 
         headline = page_soup_2.find(
-            'h1', class_='headline')  # ! giving me NoneType
-        headlines.append(headline)
+            'h1', class_='headline')
+        headlines.append(headline.text)
 
-        summary = page_soup_2.find('dd', id='abstract')
-        summaries.append(summary)
+        summary = page_soup_2.find(id='abstract')
+        summaries.append(summary.text)
 
         source = page_soup_2.find(
             'div', id='story_source').find('a')['href']
@@ -120,6 +114,7 @@ def scrape_for_search(*args):
     return final_articles
 
 
-# scrape_for_search('robot')  # for testing only
-scrape_for_top_title()
-scrape_for_top_sum()
+# scrape_for_search('thermoplastic')  # for testing only
+# scrape_for_top_title()
+# scrape_for_top_sum()
+# scrape_for_top_link()
